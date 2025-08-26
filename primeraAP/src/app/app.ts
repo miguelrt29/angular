53e4b41c8708cf1c4d6bd2b6dc25819417058cfd
miguelrt29 <miguelrt2903@gmail.com>
2025-08-26 15:23:19 -0500

    Agregando proyecto Angular como archivos normales

---
+++ primeraAP/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly saludar = signal('SOY SALUDOSO');
+  protected readonly web = signal ('pagina');
+  protected readonly saludo = signal ('saludos');
+  protected readonly kiss = signal ('Beso o besos en la mejilla');
+  protected readonly countries = signal ('paises');
+  protected readonly marca = signal ('MIGUEL');
+}
